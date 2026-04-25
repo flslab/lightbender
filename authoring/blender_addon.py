6cb87e626dacae965a5bbf043c8eff2d519417f2
Shuqin <shuqinzhu37@gmail.com>
2026-04-24 18:53:26 -0700

    addon finished

--- authoring/blender_addon.py
+++ authoring/blender_addon.py
@@ -23,7 +23,8 @@ from bpy.props import FloatProperty, StringProperty, EnumProperty, BoolProperty,
 from bpy.app.handlers import persistent
 from mathutils import Vector, Matrix
 
-ORCHESTRATOR_DIR = os.path.abspath(os.path.join(REPO_DIR, "mock_orchestrator"))
+
+REPO_DIR = "/path/to/lightbender"
 ORCHESTRATOR_DIR = os.path.abspath(os.path.join(REPO_DIR, "orchestrator"))
 AUTHORING_DIR = os.path.abspath(os.path.join(REPO_DIR, "authoring"))
 
